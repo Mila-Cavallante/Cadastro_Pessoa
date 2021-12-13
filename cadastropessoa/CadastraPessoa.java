8a63d88bbce74310dac6569e8c0ad396189486ed
Mila-Cavallante <camila.cavallante@gmail.com>
2021-12-13 06:39:33 -0300

    Update CadastraPessoa.java

--- cadastropessoa/CadastraPessoa.java
+++ cadastropessoa/CadastraPessoa.java
@@ -14,12 +14,63 @@ public class CadastraPessoa {
     List<Pessoa> pessoas = new ArrayList<>();
     
     public void addPessoa(Pessoa p){ //ponteiro
-        pessoas.add(p);
+        pessoas.add(p);//Insert
         //indice++;
     }
-    public void imprimirPessoa(){
-        for(int i=0;i<pessoas.size();i++){
-            System.out.println(pessoas.get(i).getNome()+"\n");
+    public String imprimirPessoa(){
+        String pe = " ";
+        for (Pessoa p:pessoas){
+            pe += "Nome: "+p.getNome()+ "\n";
+            pe += "Endereço: "+p.getEndereço()+ "\n";
+            pe += "Telefone: "+p.getTelefone()+ "\n";
+            pe += "Idade: "+p.getIdade()+ "\n";
         }
+        return pe;
     }
-}
+    public Pessoa buscaPessoa(String nomePessoa){//Select
+        for (Pessoa b:pessoas){
+            if(b.getNome().equals(nomePessoa)){
+                 String pe = " ";
+                        pe += "Nome: "+b.getNome()+ "\n";
+                        pe += "Endereço: "+b.getEndereço()+ "\n";
+                        pe += "Telefone: "+b.getTelefone()+ "\n";
+                        pe += "Idade: "+b.getIdade()+ "\n";
+            }
+        }
+        return null;      
+    }
+    
+    public Pessoa buscaPessoa(Pessoa p){
+        for (Pessoa x:pessoas){
+            if (x == p)
+            {
+                return x;
+            }
+        }return null;
+    }
+    
+    
+    public boolean RemovePessoa (Pessoa p){//Delete
+       Pessoa pe = buscaPessoa(p);
+        if(pe != null)
+       {
+            pessoas.remove(pe);
+            
+            return true;
+        }
+        return false;
+                
+    }
+    
+    public boolean AtualizarPessoa (Pessoa p){//Update
+        if (pessoas.contains(p)){
+         Pessoa b =buscaPessoa(p);
+         b.setNome(p.getNome());
+         b.setEndereço(p.getEndereço());
+         b.setIdade(p.getIdade());         
+         }
+        return false;
+         
+    }
+    
+}//fim classe
